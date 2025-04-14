6240ab1f3071a9630f3a2d916372e03f9c972945
cruzbeymar74 <cruzbeymar74@gmail.com>
2025-04-14 21:56:40 +0000

    Initial commit - MatchPet plataforma

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className="inicial-pagina">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="overlap-group">
+            <img className="frame" src="/Frame 2.png" alt="Frame" />
+            <div className="ellipse"></div>
+            <p className="div">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maecenas quis interdum enim enim molestie
+              faucibus. Pretium non non massa eros, nunc, urna. Ac laoreet sagittis donec vel. Amet, duis justo, quam
+              quisque egestas. Quam enim at dictum condimentum. Suspendisse.
+            </p>
+          </div>
+          <img className="logo-removebg" src="/Logo-removebg-preview 3.png" alt="Logo" /><div class="box"><div class="rectangle"><div class="text-wrapper-3">Crear una cuenta</div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
